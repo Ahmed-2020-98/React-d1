bf880413f2814c4b7e69c3eb2da452d88758b35e
Ahmed-2020-98 <ahmedgamalmansour11@gmail.com>
2022-07-30 20:40:28 +0200

    default

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './navbar/navbar';
 function App() {
   return (
     <div className="App">
+  
+    <Navbar />
     </div>
   );
 }
